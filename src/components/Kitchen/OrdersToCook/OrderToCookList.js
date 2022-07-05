f3a1fd08b7a5b6e8a4f2d82758de2ab3773b8d7c
anagarcia791 <amgt2697@gmail.com>
2022-07-05 16:47:31 -0500

    actualizacion de comentarios

--- src/components/Kitchen/OrdersToCook/OrderToCookList.js
+++ src/components/Kitchen/OrdersToCook/OrderToCookList.js
@@ -5,34 +5,8 @@ import { Card } from 'react-bootstrap';
 
 // COMPONENTE ORDERSTOCOOKLIST
 export const OrderToCookList = ({ orders, token }) => {
-  // estructura html de pedidos a preparar
-  // let ordersToCookChef = orders.map((order) => {
-  //   let statusPendingToCook;
-  //   if (order.status === 'pending') {
-  //     statusPendingToCook = (
-  //       <Card className='kitchen-orders' key={order.id.toString()}>
-  //         <div className='kitchen-orders-card-body'>
-  //           <div className='card-details-1'>
-  //             <p className='card-title'>Orden N° {order.id}</p>
-  //             <p className='card-text'>Cliente: {order.client}</p>
-  //             <p className='card-text' data-testid='date-entry'>
-  //               Hora orden: {order.dateEntry}
-  //             </p>
-  //           </div>
-  //           <div className='card-details-2'>
-  //             <p className='card-text'>Pedido:</p>
-  //             <OrderProducts products={order.products} />
-  //           </div>
-  //         </div>
-  //         <button onClick={() => statusDeliveringPetition(order.id, token)}>
-  //           Preparado
-  //         </button>
-  //       </Card>
-  //     );
-  //   }
-  //   return statusPendingToCook;
-  // });
-
+  
+  // estructura html de pedidos a preparar en cocina
   return (
     <>
       {orders
